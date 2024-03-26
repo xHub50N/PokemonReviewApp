5cdf8ae5b21cc4c5f53c25bfae817723e5766bdb
huber <hubert.bojda12@gmail.com>
2024-03-26 21:34:06 +0100

    add controller of country and category

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddControllers();
 builder.Services.TryAddTransient<Seed>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
