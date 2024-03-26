51c3d4aa6bf744ed92a04a15c80d9d19d6f9236e
huber <hubert.bojda12@gmail.com>
2024-03-26 20:11:43 +0100

    create dto of pokemon

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -1,10 +1,13 @@
-﻿namespace PokemonReviewApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonReviewApp.Models
 {
     public class Pokemon
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
+        [Required]
         public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
